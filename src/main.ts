901df194272d98667ef3c045f94c781b2959a717
Tim Neumegen <tneumegen@gmail.com>
2018-01-27 10:39:20 +1300

    Updated main

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ module MyGame {
 		
 		constructor()
 		{
-			super(800, 600, Phaser.AUTO, 'content', null);
+			super(1000
+				, 600, Phaser.AUTO, 'content', null);
 
 			this.state.add('Boot', BootState);
 			this.state.add('Preloader', PreloaderState);
