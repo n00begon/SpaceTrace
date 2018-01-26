c4c7544762060216e7ae758b8fc12bdd34f8ebc5
Tim Bennett <bennett.td@gmail.com>
2018-01-27 10:28:16 +1300

    More state logic

--- src/logic/state.ts
+++ src/logic/state.ts
@@ -1,7 +1,11 @@
+function pickOne(array: any[]) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
 type Disease = 'triangle' | 'square' | 'circle' | 'cross' | 'none';
 function pickDisease(): Disease {
     const diseases: Disease[] = ['triangle', 'square', 'circle', 'cross'];
-    return diseases[Math.floor(Math.random() * diseases.length)];
+    return pickOne(diseases);
 }
 
 type Movement = 'left' | 'right' | 'up' | 'down';
@@ -18,7 +22,9 @@ class SpaceNode {
     }
 }
 
-const MAX_SIZE = 5; // space is 5 x 5
+const MAX_SPACE_SIZE = 5; // space is 5 x 5
+const SPACE_CENTER = Math.ceil(MAX_SPACE_SIZE / 2);
+
 const STARTING_HEALTH = 5;
 
 type PlayerState = 'active' | 'defibrillate' | 'dead';
@@ -30,8 +36,18 @@ class Player {
                         // and is 'carrying' around with them
     health: number = STARTING_HEALTH;
 
-    constructor(position: Pos) {
-        this.position = position;
+    constructor() {
+        this.reposition();
+    }
+
+    reposition() { // anywhere that isn't the center
+        const legalPositions = [];
+        for (let i = 0; i < MAX_SPACE_SIZE; i++) {
+            if (i !== SPACE_CENTER)
+                legalPositions.push(i);
+        }
+        this.position.x = pickOne(legalPositions);
+        this.position.y = pickOne(legalPositions);
     }
 }
 
@@ -40,19 +56,18 @@ class State {
     space: SpaceNode[][] = [];
 
     constructor() {
-        for (let i = 0; i < MAX_SIZE; i++) {
+        for (let i = 0; i < MAX_SPACE_SIZE; i++) {
             this.space.push([]);
-            for (let j = 0; j < MAX_SIZE; j++) {
+            for (let j = 0; j < MAX_SPACE_SIZE; j++) {
                 this.space[i][j] = new SpaceNode();
             }
         }
-        this.player = new Player({x: 0, y: 0});
-        // TODO position player random somewhere that ain't the center
+        this.player = new Player();
     }
 
-    currentPositionIsValid(): boolean { // are they in the grid?
+    private currentPositionIsValid(): boolean { // are they in the grid?
         const { x, y } = this.player.position;
-        return x >= 0 && x < MAX_SIZE && y >= 0 && y < MAX_SIZE;
+        return x >= 0 && x < MAX_SPACE_SIZE && y >= 0 && y < MAX_SPACE_SIZE;
     }
 
     // this handles applying the 'special' drugs - ie. circle, triangle etc.
@@ -84,6 +99,10 @@ class State {
                 position.y++;
         }
 
+        if (this.player.state === 'defibrillate') {
+            this.player.health--; // the player needed to defibrillate, hurt them!
+        }
+
         if (!this.currentPositionIsValid()) // out of bounds, need defibrilliation!
             this.player.state = 'defibrillate';
 
@@ -95,7 +114,7 @@ class State {
 
     patientIsSaved(): boolean {
         const { x, y } = this.player.position;
-        const hasReachedCenter = x === 3 && y === 3; // hardcode this for now
+        const hasReachedCenter = x === SPACE_CENTER && y === SPACE_CENTER;
         const noDiseases = this.player.diseases.length === 0;
         return hasReachedCenter && noDiseases;
     }
